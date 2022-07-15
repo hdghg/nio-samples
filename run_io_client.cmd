c8bece30c6b79bec42949878514cdd847734a12d
Alexey Tokar <tokar.a.v@yandex.ru>
2022-07-15 17:55:19 +0300

    IO commit

---
+++ run_io_client.cmd
@@ -0,0 +1,1 @@
+start C:\soft\java-se-8u41-ri\bin\java.exe -jar ioclient/target/ioclient-1.0-SNAPSHOT.jar
---
+++ run_io_server.cmd
@@ -0,0 +1,2 @@
+start C:\soft\java-se-8u41-ri\bin\java.exe ^
+    -jar ioserv/target/ioserv-1.0-SNAPSHOT-jar-with-dependencies.jar
