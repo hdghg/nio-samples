4a6d37ea80d416fac328bae0d3dd11007e93430c
Alexey Tokar <tokar.a.v@yandex.ru>
2022-07-20 20:41:41 +0300

    Add support for console encoding

--- run_nio_client.cmd
+++ run_nio_client.cmd
@@ -1,5 +1,6 @@
 REM -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:5005
 C:\soft\java-se-8u41-ri\bin\java.exe ^
     -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:5005 ^
-    -jar nioclient/target/nioclient-1.0-SNAPSHOT-jar-with-dependencies.jar
+    -jar nioclient/target/nioclient-1.0-SNAPSHOT-jar-with-dependencies.jar ^
+    -consoleEncoding0 UTF-8
 pause
