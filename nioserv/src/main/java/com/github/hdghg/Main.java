99c554726e2b46cf66c915c8a1a4e9ce4f403f50
Alexey Tokar <tokar.a.v@yandex.ru>
2022-07-21 20:22:30 +0300

    Implement NIO based server

--- nioserv/src/main/java/com/github/hdghg/Main.java
+++
@@ -1,9 +0,0 @@
-package com.github.hdghg;
-
-import java.nio.channels.Channel;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
